ab39a45b193b5aa1ff5516d118c3be71e4577743
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-17 20:23:22 +0200

    Add header

--- index.php
+++ index.php
@@ -12,25 +12,37 @@
 
 <body class="bg_primary">
     <div id="app">
-        <div class="container">
-            <div class="row row-cols-3">
-                <!-- Itero nei dischi e creo le varie card -->
-                <div class="g-5" v-for='disk in disks'>
-                    <div class=" bg_primary_dark px-2 py-5 mx-2 my-3 shadow rounded-3 card_border">
-                        <img class="card-img-top px-5 pt-3" :src="disk.poster" alt="">
-                        <div class="card_body text-light text-center py-3">
-                            <h4 class="card-title">{{disk.title}}</h4>
-                            <div class="card-text">{{disk.author}}</div>
-                            <h5 class="card-text">{{disk.year}}</h5>
+
+        <!-- Header -->
+        <header>
+            <nav class="bg_header_dark ">
+                <i class="fa-brands fa-spotify navbar-brand text-success px-5"></i>
+            </nav>
+        </header>
+
+        <!-- Main -->
+        <main>
+            <div class="container">
+                <div class="row row-cols-3">
+                    <!-- Itero nei dischi e creo le varie card -->
+                    <div class="g-5" v-for='disk in disks'>
+                        <div class=" bg_primary_dark px-2 py-5 mx-2 my-3 shadow rounded-3 card_border">
+                            <img class="card-img-top px-5 pt-3" :src="disk.poster" alt="">
+                            <div class="card_body text-light text-center py-3">
+                                <h4 class="card-title">{{disk.title}}</h4>
+                                <div class="card-text">{{disk.author}}</div>
+                                <h5 class="card-text">{{disk.year}}</h5>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
 
     <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js' integrity='sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
     <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <script src="https://kit.fontawesome.com/ac8f8c0862.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="./assets/js/app.js"></script>
 </body>
