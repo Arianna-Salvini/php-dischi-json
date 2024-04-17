a063fc60160ae71cf2831199dee068f7c5f6b718
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-17 18:15:34 +0200

    Fix layout with bootstrap class

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,23 +13,29 @@
 <body>
     <div id="app">
         <div class="container">
-            <row class="row-cols-3">
-                <col>
-                <card>
-                    ciAO
-                </card>
-                </col>
-            </row>
+            <div class="row row-cols-3">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-img-top">
+                            <div class="card_body">
+                                <div class="card-title"></div>
+                                <div class="card-text"></div>
+                                <div class="card-text"></div>
+                                <div class="card-text"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
 
-
-
-
-    </div>
-
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="./assets/js/app.js"></script>
+        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="./assets/js/app.js"></script>
 </body>
 
 </html>
