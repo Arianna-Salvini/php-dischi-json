e85e9a7f142805b69da8d2345d3d22760566eaa5
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-17 17:51:55 +0200

    Add draft layout

--- index.php
+++ index.php
@@ -15,9 +15,21 @@
 </head>
 
 <body>
+    <div id="app">
+        <div class="container">
+            <row class="row-cols-3">
+                <col>
+                <card>
+                    ciAO
+                </card>
+                </col>
+            </row>
+        </div>
 
 
 
+
+    </div>
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
